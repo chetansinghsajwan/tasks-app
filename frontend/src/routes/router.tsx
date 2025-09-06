fd609342b30a4357899e7520b12ff39a1f6bfe4f
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-09-06 17:03:13 +0530

    update: remove wails default page, assets

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
-import App from '../App'
 
 export function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<App />} />
+                <Route path='/' element={<></>} />
             </Routes>
         </BrowserRouter>
     )
