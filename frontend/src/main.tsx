38983091165d82d39837fcacf5d4ee2364b9c564
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-09-06 17:00:59 +0530

    update: add react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './style.css'
-import App from './App'
+import { Router } from './routes/router'
 
 const container = document.getElementById('root')
 
@@ -9,6 +9,6 @@ const root = createRoot(container!)
 
 root.render(
     <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+        <Router />
+    </React.StrictMode>,
 )
