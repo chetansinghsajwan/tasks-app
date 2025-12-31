d92acb786dcb0f9d18d2a241da8140dd485bc97b
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-12-31 15:16:03 +0530

    update: add login, signup page

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,10 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import { LoginPage } from '@/pages/login'
+import { SignupPage } from '@/pages/signup'
 
 export function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<></>} />
+                <Route path='/' element={<Navigate to='/login' replace />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/signup' element={<SignupPage />} />
             </Routes>
         </BrowserRouter>
     )
